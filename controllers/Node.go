af17583cbaf2062f9e6496d59474d7f690d71e32
dz.tian <dz.tian@venusource.com>
2019-04-01 10:48:35 +0800

    Add request values for CPU and memory, limit values, and the number of pods

--- controllers/Node.go
+++ controllers/Node.go
@@ -2,6 +2,7 @@ package controllers
 
 import (
 	"fmt"
+	"strconv"
 
 	"github.com/astaxie/beego"
 	metav1 "k8s.io/apimachinery/pkg/apis/meta/v1"
@@ -13,12 +14,12 @@ type NodeController struct {
 }
 
 // 返回的pod中cpu，memory数据,pod数量
-type podData struct {
+type allocatedResources struct {
 	CpuRequests    int //cpu请求值
 	CpuLimits      int //cpu限制值
 	MemoryRequests int //内存请求值
 	MemoryLimits   int //内存限制值
-	podNum         int //pod数量
+	PodNum         int //pod数量
 }
 
 //var clientset = getClientset()
@@ -54,23 +55,48 @@ func (n *NodeController) GetByName() {
 	if err != nil {
 		panic(err)
 	}
+	// 对应的cpu请求值  内存请求值，cpu限制值 内存限制值
+	var cpuR, memoryR, cpuL, memoryL = 0, 0, 0, 0
 	for _, v := range pod.Items {
 		req := v.Spec.Containers[0].Resources.Requests
 		lim := v.Spec.Containers[0].Resources.Limits
 		if req != nil {
-			a := req["cpu"]
-			cpuR := a.String()[0 : len(a.String())-1]
-			fmt.Println(cpuR)
+			//cpuReqVal : request map集合下的所有key为cpu对应的val
+			cpuReqVal := req["cpu"]
+			//cpuR, _ = strconv.Atoi(a.String()[0 : len(a.String())-1])
+			cpu := cpuReqVal.String()[0 : len(cpuReqVal.String())-1]
+			//memoryReqVal : request map集合下的所有key为memory对应的val
+			memoryReqVal := req["memory"]
+			memory := memoryReqVal.String()[0:len(memoryReqVal.String())]
+			if memory != "0" {
+				memory = memory[0 : len(memory)-2]
+			}
+			//字符串转数字
+			Rcpu, _ := strconv.Atoi(cpu)
+			Rmemory, _ := strconv.Atoi(memory)
+			cpuR += Rcpu
+			memoryR += Rmemory
 		}
 		if lim != nil {
-
+			cpuLimVal := lim["cpu"]
+			//cpuR, _ = strconv.Atoi(a.String()[0 : len(a.String())-1])
+			cpu := cpuLimVal.String()[0 : len(cpuLimVal.String())-1]
+			memoryLimVal := lim["memory"]
+			memory := memoryLimVal.String()[0:len(memoryLimVal.String())]
+			if memory != "0" {
+				memory = memory[0 : len(memory)-2]
+			}
+			//字符串转数字
+			Lcpu, _ := strconv.Atoi(cpu)
+			Lmemory, _ := strconv.Atoi(memory)
+			cpuL += Lcpu
+			memoryL += Lmemory
 		}
 
-		fmt.Println("cpu>>>:", req["cpu"], "memory>>>:", req["memory"])
-		fmt.Println("Lcpu:", lim["cpu"], "Lmemory:", lim["memory"])
 	}
-
-	n.Data["json"] = map[string]interface{}{"data": node, "pod": pod, "code": 20000}
+	length := len(pod.Items)
+	allocatedResources := allocatedResources{CpuRequests: cpuR, CpuLimits: cpuL, MemoryRequests: memoryR, MemoryLimits: memoryL, PodNum: length}
+	n.Data["json"] = map[string]interface{}{"data": node, "pod": pod, "allocatedResources": allocatedResources, "code": 20000}
 	n.ServeJSON()
 	return
 }
